7de7cd42f623e0b212bbb2c4a5702fb4e6849bf6
ishemezwe polie <p.ishemezwe@alustudent.com>
2022-07-08 15:46:31 +0200

    we share ride

---
+++ WeShare.java
@@ -0,0 +1,26 @@
+/** 
+ Client:
+-Login or Sign up
+-Selecting origin
+-Selecting Destination
+-Selecting Date
+-Selecting Time
+-List of drivers
+-Details of a driver
+-Confirmation of the trip
+-Rating
+*/
+
+
+/** 
+Driver:
+-Login or Sign up
+-Selecting origin
+-Selecting Destination
+-Selecting Date
+-Selecting Time
+-Selcting pick up and drop off
+-List of clients available
+-Start or cancel the trip
+-Rating
+*/
